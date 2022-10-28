0acf5392fcb9cefad5143238d5a3aec94acba55e
Harsh Chandrakar <112104412+Rikashiii@users.noreply.github.com>
2022-10-28 21:11:45 +0530

    Boo!! games here.
    Here will be the GUI-less games created by me. All collaborators are happily welcomed for any upgraded features or just to make it cooler!

---
+++ caesar_game.py
@@ -0,0 +1,46 @@
+from art import logo
+print(logo)
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+#caesar function
+def caesar(text,shift,direction):
+  if direction=="encode":
+    for i in text:
+      if i in alphabet:
+        print(alphabet[alphabet.index(i)+shift],end='')
+      else:
+        print(i,end='')
+  if direction=="decode":
+    for i in text:
+      if i in alphabet:
+        print(alphabet[alphabet.index(i)-shift],end='')
+      else:
+        print(i,end='')
+#whole program control
+def program():
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
+  shift = shift%26
+  caesar(text,shift,direction)
+    #TODO-3: What happens if the user enters a number/symbol/space?
+    #Can you fix the code to keep the number/symbol/space when the text is encoded/decoded?
+    #e.g. start_text = "meet me at 3"
+    #end_text = "•••• •• •• 3"
+
+
+#TODO-1: Import and print the logo from art.py when the program starts.
+
+#TODO-4: Can you figure out a way to ask the user if they want to restart the cipher program?
+#e.g. Type 'yes' if you want to go again. Otherwise type 'no'.
+#If they type 'yes' then ask them for the direction/text/shift again and call the caesar() function again?
+#Hint: Try creating a while loop that continues to execute the program if the user types 'yes'. 
+#TODO-2: What if the user enters a shift that is greater than the number of letters in the alphabet?
+#Try running the program and entering a shift number of 45.
+#Add some code so that the program continues to work even if the user enters a shift number greater than 26. 
+#Hint: Think about how you can use the modulus (%).
+
+#main function
+again = "yes"
+while again=="yes":
+  program()
+  again = input("\nDo you want to go again?\n")
